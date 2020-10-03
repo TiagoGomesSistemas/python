681a93ccfb80d46f67e1397f0f9094562ee89a1e
tiago <tiago.gomes.sistemas@gmail.com>
2020-10-03 01:33:56 -0300

    comentarios adicionados

--- agenda/agenda.py
+++ agenda/agenda.py
@@ -2,25 +2,28 @@
 from flask import Flask, render_template, request, redirect, url_for
 from contato import Contato
 
-app = Flask(__name__)
-
 # Main #
+app = Flask(__name__)
 
 contatos = []
 
+# index.html #
 @app.route('/')
 def index():
     return render_template('lista.html', titulo='Agenda', agenda=contatos)
 
+# novo.html #
 @app.route('/novo')
 def novo():
     return render_template('novo.html', titulo='Novo Contato')
 
+# contato.html #
 @app.route('/contato', methods=['POST',])
 def contato():
     acesso = request.form['acessar']
     return render_template('contato.html', titulo='Contato', acesso=acesso, contatos=contatos)
 
+# Adiciona novo endereço e retorna para index.html #
 @app.route('/endereco', methods=['POST',])
 def endereco():
     nome = request.form['nome']
@@ -31,7 +34,7 @@ def endereco():
             item.consulta(cep, complemento)
     return redirect(url_for('index'))
 
-
+# Cria novo contato e retorna para index.html #
 @app.route('/criar', methods=['POST',])
 def criar():
     id = len(contatos)
@@ -42,5 +45,5 @@ def criar():
     contatos.append(contato)
     return redirect(url_for('index'))
 
-
+# Chamada do app #
 app.run(debug=True)
